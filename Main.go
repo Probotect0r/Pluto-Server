003059a79af3814f4c11dee4629b081abb11cf08
Probotect0r <sagar.desai@uoit.net>
2016-11-14 00:56:19 -0500

    Added main file.

--- Main.go
+++ Main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import "net"
 
 func main() {
-	fmt.Println("Hello world!")
+	addr, error := net.ResolveUDPAddr("udp", ":9000")
+
+	if error != nil {
+		panic(error)
+	}
+	conn, err := net.ListenUDP("udp", addr)
+
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func handleConnection(conn net.Conn) {
+
 }
